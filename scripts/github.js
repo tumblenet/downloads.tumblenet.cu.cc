2fe15b467f258309b8103293590ab63348950fa7
Tumble <tumblegamer@tumblenet.tk>
2016-07-04 13:06:45 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
       $("#repos").html("loading GitHub API...");
       var text = "";
       githubAPI("users/tumblegamer/repos", function(data) {
-            text=data[0].name;
+            text=data[0].id;
       });
       $("#repos").html(text);
       
