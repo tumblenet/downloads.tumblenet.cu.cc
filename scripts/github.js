403a529e07bfa1ba0da7444c98e1e54b5287df0e
Tumble <tumblegamer@tumblenet.tk>
2016-07-04 14:11:26 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -10,7 +10,7 @@ jQuery.fn.loadRepositories = function(username) {
             if (this.name != (username.toLowerCase()+'.github.io')) {
                 text = text + "<section class='section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp'><div class='mdl-card mdl-cell mdl-cell--12-col'><div class='mdl-card__supporting-text'><h4>" + this.name + "</h4>" + this.description + "</div><div class='mdl-card__actions'><a href='" + this.html_url + "' class='mdl-button'>GitHub</a>";
                 if(this.homepage != ""){
-                    text=text + "<a href='" + this.homepage + "' class='mdl-button'>Website</a>"
+                    text=text + "<a href='" + this.homepage + "' class='mdl-button'>Website</a>";
                 }
                 text= text + "</div></div></section>";
             }
