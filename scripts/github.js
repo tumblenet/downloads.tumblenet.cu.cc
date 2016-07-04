d42a9fb4716bf23ec55e7a811f33fd21ef5dac5f
Tumble <tumblegamer@tumblenet.tk>
2016-07-04 13:28:31 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -10,8 +10,7 @@ jQuery.fn.loadRepositories = function(username) {
         target.empty().append(list);
         $(repos).each(function() {
             if (this.name != (username.toLowerCase()+'.github.io')) {
-                list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + this.description +'</dd>');
+                alert(this.id);
             }
         });      
       });
