e5fa0cd77cc100010bd63387029b90ba1d0c8670
Tumble <tumblegamer@tumblenet.tk>
2016-07-04 14:14:00 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -5,7 +5,7 @@ jQuery.fn.loadRepositories = function(username) {
     var text = "";
     githubAPI("users/" + username + "/repos", function(data) {
         var repos = data.data; // JSON Parsing
-        //sortByName(repos);    
+        sortByName(repos);    
         $(repos).each(function() {
             if (this.name != (username.toLowerCase()+'.github.io')) {
                 text = text + "<section class='section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp'><div class='mdl-card mdl-cell mdl-cell--12-col'><div class='mdl-card__supporting-text'><h4>" + this.name + "</h4>" + this.description + "</div><div class='mdl-card__actions'><a href='" + this.html_url + "' class='mdl-button'>GitHub</a>";
@@ -18,10 +18,10 @@ jQuery.fn.loadRepositories = function(username) {
         target.html(text);
     });
       
-    /*function sortByName(repos) {
+    function sortByName(repos) {
         repos.sort(function(a,b) {
         return a.name - b.name;
        });
-    }*/
+    }
 };
 
