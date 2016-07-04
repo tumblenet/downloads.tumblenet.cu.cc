7718de3b8a5d188da57eb2e22f667cbaed64ab8c
Tumble <tumblegamer@tumblenet.tk>
2016-07-04 10:43:31 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
       $("#repos").html("test");
-      var text = "";
-      var reposArray=[];
-      reposArray = githubAPI("orgs/tumblegamer/repos", function(data) {
+      
+      githubAPI("orgs/tumblegamer/repos", function(data) {
+            var text = "";
             $.each(data, function(index,element) {
           text = text + "<li>" + element.name + "<br><a href=" + element.zipball_url + ">Download ZIP</a> | <a href=" + element.tarball_url + ">Download TAR</a> | <a href=" + element.html_url + ">Change Log</a></li><br>";
+          text = "<ul>" + text + "</ul>";
+          $("#repos").html(text);
       });
       });
-      text = "<ul>" + text + "</ul>";
-      alert(text);
-      alert(releasesText);
       
-      $("#repos").html(text);
+      
+      
     });
