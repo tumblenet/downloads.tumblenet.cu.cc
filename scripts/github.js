60ad00ce68900f09d8f5a4c2bcbed4f0232fc612
Tumble <tumblegamer@tumblenet.tk>
2016-10-20 13:37:39 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,5 +1,5 @@
 jQuery.fn.loadRepositories = function(username) {
-    this.html("<span>API rate limit exceeded. (But here's the good news: Authenticated requests get a higher rate limit. <a href='https://developer.github.com/v3/#rate-limiting'>Check out the documentation for more details.</a>)</span>");
+    this.html("<span>API rate limit exceeded. (But here's the good news: Authenticated requests get a higher rate limit. <a href='https://developer.github.com/v3/#rate-limiting'>Check out the documentation for more details.</a>)</span><br><span>Normal Users: we are sorry that we cannot connect to githubs servers to get the information. <a href='http://github.com/" + username + "'>Click here to see them directly on GitHub's Servers.</a></span>");
      
     var target = this;
     var text = "";
